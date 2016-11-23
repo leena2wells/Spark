d53a5ea3ff84639b3299e93051b8c969771e69d9
Sunil Patil <sunilpatil@maprtech.com>
2016-11-23 10:20:42 -0800

    Changed Spark program to take input on command line

--- src/main/scala/org/spnotes/spark/HelloSparkElasticSearch.scala
+++ src/main/scala/org/spnotes/spark/HelloSparkElasticSearch.scala
@@ -8,15 +8,26 @@ import org.elasticsearch.spark._
   */
 object HelloSparkElasticSearch {
 
-  def main(argv:Array[String]): Unit ={
+  def main(argv: Array[String]): Unit = {
+    if(argv.length != 4){
+      println("Usage Pattern: HelloSparkElasticSearch <inputfilepath> <eshostname> <espost> <esresource>")
+      return
+    }
+    val filePath = argv(0)
+    val esHost = argv(1)
+    val esPort = argv(2)
+    val esResource = argv(3)
+
+
     val sparkConf = new SparkConf().setAppName("FileReader").setMaster("local[*]")
     val sparkContext = new SparkContext(sparkConf);
 
-    val lines = sparkContext.textFile("/Users/sunilpatil/software/spark-1.6.2-bin-hadoop2.6/README.md")
-    val wordCount = lines.flatMap(line => line.split(" ")).map(word => (word,1)).reduceByKey((x,y)=> x+y)
+    val lines = sparkContext.textFile(filePath)
+    val wordCount = lines.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((x, y) => x + y)
 
     wordCount.foreach(println)
-    val esMap = Map("es.nodes"->"localhost", "es.port"->"9200", "es.resource"->"wordcount/counts" )
+//    val esMap = Map("es.nodes" -> "localhost", "es.port" -> "9200", "es.resource" -> "wordcount/counts")
+val esMap = Map("es.nodes" -> esHost, "es.port" -> esPort, "es.resource" -> esResource)
     wordCount.saveToEs(esMap)
   }
 }
